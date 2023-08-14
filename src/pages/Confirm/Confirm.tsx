9553d2b50524bab48f1983705c3f635b998d79a4
home <catalinpce@gmail.com>
2023-08-14 13:48:11 +0300

    added docs on confirm

--- src/pages/Confirm/Confirm.tsx
+++ src/pages/Confirm/Confirm.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Container from "../../components/ui/Container";
 import Button from "../../components/ui/Button";
+import ArticleIcon from "@mui/icons-material/Article";
 import { useNavigate } from "react-router-dom";
 
 export default function Confirm() {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-grow flex-col items-center justify-center">
+    <div className="flex flex-grow flex-col items-center gap-24 justify-between">
       <Container className="text-start flex flex-col">
-        <label className="text-4xl lg:text-6xl text-primary font-semibold">
+        <label className="text-4xl lg:text-6xl text-primary font-semibold pt-24 lg:pt-36">
           Te-ai inscris cu succes!
         </label>
         <label className="text-base lg:text-lg opacity-80 ms-2">
@@ -20,6 +21,28 @@ export default function Confirm() {
           </Button>
         </div>
       </Container>
+      <div className="bg-neutral-100 py-4 w-full -mb-0.5 z-[10] font-semibold">
+        <Container className="grid grid-cols-3 justify-between">
+          <div className="flex flex-col gap-2 items-center text-primary font-semibold cursor-pointer hover:text-primary/50 transition-all">
+            <div className="text-6xl lg:text-8xl">
+              <ArticleIcon fontSize="inherit" />
+            </div>
+            <label>Terms & Conditions</label>
+          </div>
+          <div className="flex flex-col gap-2  items-center text-primary font-semibold cursor-pointer hover:text-primary/50 transition-all">
+            <div className="text-6xl lg:text-8xl">
+              <ArticleIcon fontSize="inherit" />
+            </div>
+            <label>GDPR</label>
+          </div>
+          <div className="flex flex-col gap-2 items-center text-primary font-semibold cursor-pointer hover:text-primary/50 transition-all">
+            <div className="text-6xl lg:text-8xl">
+              <ArticleIcon fontSize="inherit" />
+            </div>
+            <label>Jurizare</label>
+          </div>
+        </Container>
+      </div>
     </div>
   );
 }
