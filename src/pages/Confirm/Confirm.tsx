cccbaf766efdca30217a1fbff9bba57a18a13f39
home <catalinpce@gmail.com>
2023-08-24 20:14:21 +0300

    docs added

--- src/pages/Confirm/Confirm.tsx
+++ src/pages/Confirm/Confirm.tsx
@@ -22,25 +22,29 @@ export default function Confirm() {
         </div>
       </Container>
       <div className="bg-neutral-100 py-4 w-full -mb-0.5 z-[10] font-semibold">
-        <Container className="grid grid-cols-3 justify-between">
-          <div className="flex flex-col gap-2 items-center text-primary font-semibold cursor-pointer hover:text-primary/50 transition-all">
+        <Container className="flex flex-row justify-between">
+          <a
+            target="_blank"
+            href="https://documente.code-envision.ro/termeni_conditii.pdf"
+            className="flex w-fit flex-col gap-2 items-center text-primary font-semibold cursor-pointer hover:text-primary/50 transition-all"
+            rel="noreferrer"
+          >
             <div className="text-6xl lg:text-8xl">
               <ArticleIcon fontSize="inherit" />
             </div>
-            <label>Regulament Concurs</label>
-          </div>
-          <div className="flex flex-col gap-2  items-center text-primary font-semibold cursor-pointer hover:text-primary/50 transition-all">
+            <label>Termeni si Conditii</label>
+          </a>
+          <a
+            target="_blank"
+            href="https://documente.code-envision.ro/descriere_subiect.pdf"
+            className="flex w-fit flex-col gap-2  items-center text-primary font-semibold cursor-pointer hover:text-primary/50 transition-all"
+            rel="noreferrer"
+          >
             <div className="text-6xl lg:text-8xl">
               <ArticleIcon fontSize="inherit" />
             </div>
-            <label>Detalii Subiect</label>
-          </div>
-          <div className="flex flex-col gap-2 items-center text-primary font-semibold cursor-pointer hover:text-primary/50 transition-all">
-            <div className="text-6xl lg:text-8xl">
-              <ArticleIcon fontSize="inherit" />
-            </div>
-            <label>Politica Confidentialitate</label>
-          </div>
+            <label>Descriere Subiect</label>
+          </a>
         </Container>
       </div>
     </div>
